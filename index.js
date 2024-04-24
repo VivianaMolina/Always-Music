a331159bce64477274fee943093361ef235b37ce
Viviana Molina <viviana.a.molina@gmail.com>
2024-04-24 03:05:41 -0400

    Added arguments to evaluate what action will be executed: select, update, delete, insert

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const { Pool } = require("pg");
 const { database } = require('pg/lib/defaults');
-const argumentos = process.argv.slice(1);
+const argumentos = process.argv.slice(2);
+const evento = String(argumentos[0]);
+const nombre = String(argumentos[1]);
+const rut = String(argumentos[2]);
+const curso = String(argumentos[3]);
+const nivel = String(argumentos[4]);
 
 const config = {
     host: "localhost",
@@ -11,20 +16,22 @@ const config = {
 };
 const pool = new Pool(config);
 
+
 //1. Crear una función asíncrona para registrar un nuevo estudiante en la base de datos.
-const insertstudents = async () => {
+const insertstudents = async (nombre, rut, curso, nivel) => {
     try {
         const text = "insert into students (nombre, rut, curso, nivel) values ($1, $2, $3, $4)";
-        const values = ["Viviana Molina", "15.440.720-0", "Math", "10"];
+        const values = [nombre, rut, curso, nivel];
         const response = await pool.query(text, values);
-        console.log(response);
+        console.log(`Estudiante ${nombre} agregado con éxito`, response);
 
     } catch (error) {
         console.error('Error al insertar el registro:', error.message);
         throw error;
-    } 
+    } finally {
+        pool.end();
+    }
 };
-insertstudents();
 
 // 2. Crear una función asíncrona para obtener por consola el registro de un estudiante por medio de su rut.
 const getStudentRecordbyRut = async (rut) => {
@@ -39,56 +46,54 @@ const getStudentRecordbyRut = async (rut) => {
     } catch (error) {
         console.error('Error al obtener el registro:', error.message);
         throw error;
-    } 
+    } finally {
+        pool.end();
+    }
 }
 
-const rutStudent = String(argumentos[1]);
-getStudentRecordbyRut(rutStudent);
-
-
 // 3. Crear una función asíncrona para obtener por consola todos los estudiantes registrados
-const getRegisteredStudents= async () => {
+const getRegisteredStudents = async () => {
     try {
         const registro = await pool.query("SELECT * FROM students");
 
         // Muestra el registro por consola
-        console.log('Los estudiates registrados son:', registro);
+        console.log('registro actual', registro);
     } catch (error) {
         console.error('Error al obtener los registros:', error.message);
         throw error;
+    } finally {
+        pool.end();
     }
 }
 
-getRegisteredStudents();
-
 // 4. Crear una función asíncrona para actualizar los datos de un estudiante en la base de datos.
-const updateStudent= async () => {
+const updateStudent = async (nombre, rut, curso, nivel) => {
     try {
-        const text = "UPDATE students SET nombre = $2, rut = $3, curso = $4, nivel = $5 where id = $1";
-        const values = [1, "Ema", "12.456.987-2", "science", "4"];
+        const text = "UPDATE students SET nombre = $1, rut = $2, curso = $3, nivel = $4 where rut = $2";
+        const values = [nombre, rut, curso, nivel];
 
         const registro = await pool.query(text, values);
 
         // Muestra el registro por consola
-        console.log('Actualización del estudiante:', registro);
+        console.log(`Estudiante ${nombre} editado con éxito`, registro);
     } catch (error) {
         console.error('Error al actualizar el registro:', error.message);
         throw error;
-    } 
+    } finally {
+        pool.end();
+    }
 }
 
-updateStudent();
-
 // 5. Crear una función asíncrona para eliminar el registro de un estudiante de la base de datos.
-const deleteStudent= async () => {
+const deleteStudent = async (rut) => {
     try {
-        const text = "DELETE FROM students where id = $1";
-        const values = [2];
+        const text = "DELETE FROM students where rut = $1";
+        const values = [rut];
 
         const registro = await pool.query(text, values);
 
         // Muestra el registro por consola
-        console.log('Se elimino al estudiante:', registro);
+        console.log(`Registro de estudiante con rut ${rut} eliminado`, registro);
     } catch (error) {
         console.error('Error al eliminar el registro:', error.message);
         throw error;
@@ -97,4 +102,22 @@ const deleteStudent= async () => {
     }
 }
 
-deleteStudent();
+switch (evento) {
+    case "nuevo":
+        insertstudents(nombre, rut, curso, nivel);
+        break;
+    case "consulta":
+        getRegisteredStudents();
+        break;
+    case "editar":
+        updateStudent(nombre, rut, curso, nivel);
+        break;
+    case "rut":
+        getStudentRecordbyRut(rut);
+        break;
+    case "eliminar":
+        deleteStudent(rut);
+        break;
+    default:
+        console.log("ingrese parametros validos");
+}
